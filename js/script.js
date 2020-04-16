f396ec9180e2170eee0ea6728a99259d6684de7a
Aleksandr Lyapin <konig037@yandex.ru>
2020-04-16 18:30:33 +0300

    Исправить ошибки, размер фото профиля взят по высоте, т.к. сама картинка шире

--- js/script.js
+++ js/script.js
@@ -2,29 +2,30 @@ const popup = document.querySelector('.popup');
 const editButton = document.querySelector('.profile__edit-button');
 let profileTitle = document.querySelector('.profile__title');
 let profileSubtitle = document.querySelector('.profile__subtitle');
-let popupTitle = document.querySelector('.popup__title');
-let popupSubtitle = document.querySelector('.popup__subtitle');
+let popupTitle = document.querySelector('.popup__text_title');
+let popupSubtitle = document.querySelector('.popup__text_subtitle');
+const exitButton = document.querySelector('.popup__button-exit');
+const formElement = document.querySelector('.popup__container');
 
 function popupShow() {
     popupTitle.value = profileTitle.textContent;
     popupSubtitle.value = profileSubtitle.textContent;
     popup.classList.add('popup_opened');
 }
-editButton.addEventListener('click', popupShow);
 
-const exitButton = document.querySelector('.popup__button-exit');
 function popupExit() {
     popup.classList.remove('popup_opened');
 }
-exitButton.addEventListener('click', popupExit);
 
-const formElement = document.querySelector('.popup__container');
 function formSubmitHandler (evt) {
     evt.preventDefault();
     profileTitle.textContent = popupTitle.value;
     profileSubtitle.textContent = popupSubtitle.value;
     popupExit();
 }
+
 formElement.addEventListener('submit', formSubmitHandler);
+editButton.addEventListener('click', popupShow);
+exitButton.addEventListener('click', popupExit);
 
 
